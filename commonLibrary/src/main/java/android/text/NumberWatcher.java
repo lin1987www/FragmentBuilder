dcbeca8696e35e321fd309c8a7ab4286dd5cb073
林奕忠 <lin1987www@gmail.com>
2017-09-14 23:31:09 +0800

    v26.0.2.4

---
+++ commonLibrary/src/main/java/android/text/NumberWatcher.java
@@ -0,0 +1,112 @@
+package android.text;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Administrator on 2017/9/14.
+ */
+
+public class NumberWatcher implements TextWatcher {
+    private final static Pattern removeZerosPattern = Pattern.compile("(?:0+?)([123456789]+(?:\\.)?(?:\\d+)?)$");
+
+    private int decimalPlaces = 0;
+    private Pattern decimalPlacesPattern;
+    private double min, max, defaultValue;
+    private String newText;
+
+
+    public void set(double min, double max, double defaultValue, int decimalPlaces) {
+        this.min = min;
+        this.max = max;
+        this.defaultValue = defaultValue;
+        this.decimalPlaces = decimalPlaces;
+        decimalPlacesPattern = Pattern.compile("(\\d+\\.\\d{" + String.valueOf(Math.max(0, decimalPlaces)) + "})(?:\\d+)");
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s,
+                                  int start,
+                                  int count,
+                                  int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s,
+                              int start,
+                              int before,
+                              int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        SpannableStringBuilder spannableStringBuilder = (SpannableStringBuilder) editable;
+        String text = spannableStringBuilder.toString();
+        String tempText = text;
+
+        if (newText != null) {
+            if (newText.equals(text)) {
+                newText = null;
+                return;
+            } else {
+                return;
+            }
+        }
+
+        double value = defaultValue;
+        try {
+            value = Double.parseDouble(text);
+        } catch (Throwable throwable) {
+            tempText = shortNumberString(value);
+        }
+
+        if (min <= value && value <= max) {
+            // OK
+            // 拿掉數字前多餘的0
+            tempText = removeZeros(tempText);
+            // decimalPlacesPattern  取小數點第幾位
+            tempText = toDecimalPlaces(tempText);
+            // Final
+            if (!tempText.equals(text)) {
+                newText = tempText;
+            }
+        } else if (min > value) {
+            newText = shortNumberString(min);
+        } else if (max < value) {
+            newText = shortNumberString(max);
+        }
+        if (newText != null) {
+            spannableStringBuilder.replace(0, spannableStringBuilder.length(), newText);
+        }
+    }
+
+    private String shortNumberString(double value) {
+        long valueLong = Double.valueOf(max).longValue();
+        String shortNumberString = String.valueOf(Math.ceil(valueLong) == value ? valueLong : value);
+        shortNumberString = toDecimalPlaces(shortNumberString);
+        return shortNumberString;
+    }
+
+    private String removeZeros(String text) {
+        Matcher matcher;
+        // 拿掉數字前多餘的0
+        matcher = removeZerosPattern.matcher(text);
+        if (matcher.matches()) {
+            text = matcher.replaceAll("$1");
+        }
+        return text;
+    }
+
+    private String toDecimalPlaces(String text) {
+        if (decimalPlacesPattern != null) {
+            Matcher matcher;
+            // decimalPlacesPattern  取小數點第幾位
+            matcher = decimalPlacesPattern.matcher(text);
+            if (matcher.matches()) {
+                text = matcher.replaceAll("$1");
+            }
+        }
+        return text;
+    }
+}
